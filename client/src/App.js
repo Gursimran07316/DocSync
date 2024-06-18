c30cb42999af6407d8ea5d7650d0a4c3e781579d
Gursimrandeep Singh Khela <60434846+Gursimran07316@users.noreply.github.com>
2024-06-18 06:28:02 -0400

    Backend and socket setup"

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
-import EditorScreen from "./Screens/EditorScreen";
+
+import './App.css';
+
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { v4 as uuid } from 'uuid';
+import EditorScreen from './Screens/EditorScreen';
 
 
 function App() {
   return (
-    <>
-      <EditorScreen/>
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Navigate replace to={`/docs/${uuid()}`} /> } />
+        <Route path='/docs/:id' element={<EditorScreen />} />
+      </Routes>
+    </Router>
   );
 }
 
